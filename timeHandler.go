f1bb1f848370334c51f22be9e500ddb959f19553
OlegKorniychuk <kornoleg576@gmail.com>
2023-02-20 11:18:28 +0200

    added check for error

--- timeHandler.go
+++ timeHandler.go
@@ -8,6 +8,14 @@ import (
 )
 
 func TimeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+    http.Error(w, "Method is not supported, use GET request", http.StatusNotFound)
+    return
+    } else if r.URL.Path != "/time" {
+    http.Error(w, "404 page not found", http.StatusNotFound)
+    return
+  }
+	
 	current := time.Now().Format(time.RFC3339)
 	response := map[string]string{"time": current}
 	w.Header().Set("Content-Type", "application/json")
