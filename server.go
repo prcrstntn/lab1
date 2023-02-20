c6cc32c95722fbecb96613b121c9513f4c390f8c
Dimdim28 <wwwdimanes1@gmail.com>
2023-02-20 10:55:57 +0200

    created server

--- server.go
+++ server.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"fmt"
+	"log"
+)
 
+func main() {
+	fmt.Printf("Starting server at port :8795")
+	err := http.ListenAndServe(":8795", nil)
+	if err != nil {
+	  log.Fatal(err)
+	}
 }
